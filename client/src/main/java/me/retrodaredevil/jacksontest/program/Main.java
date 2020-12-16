b1a624ba7a48c3f02cb8539d63302e24038de858
Joshua Shannon <retrodaredevil@gmail.com>
2020-12-15 23:20:39 -0600

    Small fix

--- client/src/main/java/me/retrodaredevil/jacksontest/program/Main.java
+++ client/src/main/java/me/retrodaredevil/jacksontest/program/Main.java
@@ -1,7 +1,6 @@
 package me.retrodaredevil.jacksontest.program;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.time.Duration;
@@ -14,8 +13,8 @@ public final class Main {
 	public static int doMain(String[] args){
 		System.out.println("Result of ObjectMapper.findModules():");
 		System.out.println(ObjectMapper.findModules());
-		Object thing = MapperFeature.ALLOW_VOID_VALUED_PROPERTIES; // This is here to prove that we're using 2.12
-		System.out.println(thing);
+//		Object thing = MapperFeature.ALLOW_VOID_VALUED_PROPERTIES; // This is here to prove that we're using 2.12
+//		System.out.println(thing);
 		try {
 			System.out.println(MAPPER.readValue("\"PT5M\"", Duration.class));
 		} catch (JsonProcessingException e) {
